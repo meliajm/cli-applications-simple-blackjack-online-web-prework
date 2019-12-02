dcdf1d3f80c2e5cffa39f59e187467828a9d4c20
melia miller <meliajm@gmail.com>
2019-12-02 01:53:04 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def welcome
   # code #welcome here
   puts "Welcome to the Blackjack Table"
@@ -5,8 +7,7 @@ end
 
 def deal_card
   # code #deal_card here
-  card = rand(1..11)
-  card
+  rand(1..11)
 end
 
 def display_card_total(total)
@@ -21,23 +22,51 @@ end
 
 def get_user_input
   # code #get_user_input here
+  gets.chomp.strip
 end
 
-def end_game
+def end_game(z)
   # code #end_game here
+  
+  puts "Sorry, you hit #{z}. Thanks for playing!"
+
 end
 
 def initial_round
   # code #initial_round here
-end
-
-def hit?
-  # code hit? here
+  a = deal_card
+  b = deal_card
+  sum = a + b
+  display_card_total(sum)
+  sum
+  
+  
 end
 
 def invalid_command
   # code invalid_command here
+  puts "Please enter a valid command"
 end
+
+def hit?(card_total)
+  # code hit? here
+  prompt_user
+  a = get_user_input
+  
+  if a == 'h'
+    card_total += deal_card
+  elsif a == 's'
+    card_total
+  
+  else
+    invalid_command
+    hit?(card_total)
+  end
+  card_total
+  
+end
+
+
 
 #####################################################
 # get every test to pass before coding runner below #
@@ -45,5 +74,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  t = initial_round
+  until t > 21
+    t = hit?(t)
+    display_card_total(t)
+  end
+  end_game(t) 
 end
     
